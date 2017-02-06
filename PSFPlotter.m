ea607831ed3e451be63d2a08430fab0b935cd70b
Brandon Dube <brandondube@gmail.com>
2017-02-06 01:32:49 -0500

    new PSF plotting module

---
+++ PSFPlotter.m
@@ -0,0 +1,75 @@
+classdef PSFPlotter
+    methods (Static)
+        function [] = plot3D(AberrationSwissArmyKnife, plotType)
+            %plot 3D rendition of PSF
+            
+            if nargin < 2
+                plotType = 'surf';
+            end
+            checkPSF(AberrationSwissArmyKnife);
+
+            figure;
+            axis = AberrationSwissArmyKnife.psfAxis;
+            [U,V] = meshgrid(axis, axis');
+
+            pt = lower(plotType);
+            switch pt
+                case 'mesh'
+                    mesh(U,V,AberrationSwissArmyKnife.psf);
+                case 'surf'
+                    surf(U,V,AberrationSwissArmyKnife.psf, 'EdgeColor', 'none');
+                    shading interp;
+            end
+            xlim([-4 4]);
+            ylim([-4 4]);
+            xlabel('\lambdaN'); ylabel('\lambdaN');
+            zlabel('PSF Rel. Intensity');
+        end
+
+        function [] = plotSliceX(AberrationSwissArmyKnife)
+            %plots slice of the PSF through the X axis
+
+            checkPSF(AberrationSwissArmyKnife);
+
+            plot(AberrationSwissArmyKnife.psfAxis, AberrationSwissArmyKnife.psfSlice,'.-');
+            xlabel('\lambdaN');
+            ylabel('Relative Intensity');
+            xlim([-4 4]);
+            grid on
+        end
+
+        function [] = plotSliceY(AberrationSwissArmyKnife)
+            %plots slice of the PSF through the Y axis
+
+            checkPSF(AberrationSwissArmyKnife);
+
+            plot(AberrationSwissArmyKnife.psfAxis, AberrationSwissArmyKnife.psfSlice, '.-');
+            xlabel('\lambdaN');
+            ylabel('Relative Intensity');
+            xlim([-4 4]);
+            grid on
+        end
+
+        function [] = plotSliceXY(AberrationSwissArmyKnife)
+            %plots slice through both the X and Y axis of the pupil
+
+            figure;
+            hold on
+            plot(AberrationSwissArmyKnife.psfAxis, AberrationSwissArmyKnife.psfSlice, '.-');
+            plot(AberrationSwissArmyKnife.psfAxis, AberrationSwissArmyKnife.psfSlice, '.-');
+            xlabel('\lambdaN');
+            ylabel('Relative Intensity');
+            xlim([-4 4]);
+            grid on
+        end
+    end
+end
+
+function [] = checkPSF(AberrationSwissArmyKnife)
+    if (isempty(AberrationSwissArmyKnife.psf))
+        if (isempty(AberrationSwissArmyKnife.w))
+            AberrationSwissArmyKnife.buildPupil();
+        end
+        AberrationSwissArmyKnife.w2psf();
+    end
+end
