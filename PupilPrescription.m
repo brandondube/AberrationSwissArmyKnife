233b0019d72af22ae231ee6aeb0f8b6962daffb5
Brandon Dube <brandondube@gmail.com>
2017-02-12 20:00:19 -0500

    modernize the pupil prescription for a simpler API

--- PupilPrescription.m
+++ PupilPrescription.m
@@ -1,26 +1,22 @@
 classdef PupilPrescription < handle
     %PUPILPRESCRIPTION aberration description of a pupil
-    
+
     properties
         notation
-        seidelTerms
-        seidelCoefficients
-        zernikeTerms
-        zernikeCoefficients
+        terms
+        coefficients
         centralObscuration
     end
-    
+
     methods
         function obj = PupilPrescription(varargin)
             % parse inputs
             p = inputParser;
             p.KeepUnmatched = false;
-            p.addParameter('notation',            'Z',     @ischar);
-            p.addParameter('seidelTerms',         [040],   @isnumeric);
-            p.addParameter('seidelCoefficients',  [0.125], @isnumeric);
-            p.addParameter('zernikeTerms',        [8],     @isnumeric);
-            p.addParameter('zernikeCoefficients', [0.125], @isnumeric);
-            p.addParameter('centralObscuration',  0,       @isnumeric);
+            p.addParameter('notation',           'Z',     @ischar);
+            p.addParameter('terms',              [040],   @isnumeric);
+            p.addParameter('coefficients',       [0.000], @isnumeric);
+            p.addParameter('centralObscuration',  0,      @isnumeric);
             p.parse(varargin{:});
 
             fields = fieldnames(p.Results);
@@ -28,17 +24,9 @@ classdef PupilPrescription < handle
                 obj.(fields{i}) = p.Results.(fields{i});
             end
         end
-        function [] = setSeidel(terms, coefficients)
-            obj.seidelTerms = terms;
-            obj.seidelCoefficients = coefficients;
-        end
-        function [] = setZernike(terms, coefficients)
-            obj.zernikeTerms = terms;
-            obj.zernikeCoefficients = coefficients;
-        end
         function [] = setObscuration(linearPercent)
             obj.centralObscuration = linearPercent;
         end
     end
-    
+
 end
